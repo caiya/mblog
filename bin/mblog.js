24a5de37699646bb2040ef5406d11194729186fe
caiya928 <username@macaiya928@aliyun.com>
2016-02-21 21:31:54 +0800

    初始化项目

---
+++ bin/mblog.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+/**
+ * @author 晁州
+ * @type {Command|*|exports|module.exports}
+ */
+
+var program = require("commander");
+
+program.version("0.0.1");
+
+//help
+program
+    .command("help")
+    .description("帮助")
+    .action(function(){
+        program.outputHelp();
+    });
+
+//create
+program
+    .command("create [dir]")
+    .description("创建一个空白博客")
+    .action(function(dir){
+        console.log("create %s",dir);
+    });
+
+//preview
+program
+    .command("preview [dir]")
+    .description("实时预览")
+    .action(require("../lib/cmd_preview.js"));
+
+//build
+program
+    .command("build [dir]")
+    .description("生成全站静态html")
+    .option("-o,--output <dir>","生成的静态html存放目录")
+    .action(function (dir,option) {
+        console.log("build %s,output %s",dir,option.output);
+    });
+
+//start
+program.parse(process.argv);
